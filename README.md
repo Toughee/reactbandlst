ee2617f7b352807345a85c65dd49bc1c3127952a
Toughee <Jimernez.io@gmail.com>
2021-11-12 10:36:31 -0600

    reactbandlist/001/Initial_Front_End - Added Front End U.I. and React framework to app - Updated app README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # reactbandlst
-List generator for bands by genre
+Aggregator app for bands in different categories. 
+
+- Most popular artists 
+- Most listened to songs
+- Greatests songs of all times
+
+## Current roadmap
+
+Front end portion of app is mostly completed. Back end is currently being worked on. Final app will feature real time data extracted from an music catalog API.
---
+++ src/App.js
@@ -0,0 +1,76 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <h1>react band tabs</h1>
+      </header>
+
+    <div className="Container">
+    <div className="row">
+      <section className="col">
+      <h2>Top artists</h2>
+      <ol>
+      <li>Ed Sheeran</li>
+      <li>Drake</li>
+      <li>Adele</li>
+      <li>Doja Cat</li>
+      <li>Olivia Rodrigo</li>
+      <li>The Weeknd</li>
+      <li>Luke Combs</li>
+      <li>Dua Lipa</li>
+      <li>Justin Bieber</li>
+      <li>Lil Nas X</li>
+
+
+      </ol>
+      </section>
+
+      <section className="col">
+      <h2>Top 100</h2>
+      <ol>
+      <li>Easy on Me - Adele</li>
+      <li>Stay - The Kid LAROI & Justin Bieber</li>
+      <li>Industry Baby - Lil Nas</li>
+      <li>Bad Habits - Ed Sheeran</li>
+      <li>Fancy Like - Walker Hayes</li>
+      <li>Shivers - Ed Sheeran</li>
+      <li>Way 2 Sexy - Drake</li>
+      <li>Need To Know -  Doja Cat</li>
+      <li>Good 4 U - Olivia Rodrigo</li>
+      <li>Heat Waves - Glass Animals</li>
+
+
+      </ol>
+
+      </section>
+
+      <section className="col">
+      <h2>Greatest songs of all time</h2>
+      <ol>
+      <li>The Twist - Chubby Checker</li>
+      <li>Smooth - Santana</li>
+      <li>Mack The Knife - Bobby Darin</li>
+      <li>How Do I Live - LeAnn Rimes</li>
+      <li>Party Rock Anthem - LMFAO</li>
+      <li>I Gotta Feeling - The Black Eyed Peas</li>
+      <li>Macarena - Los Del Rio</li>
+      <li>Physical - Olivia Newton-John</li>
+      <li>You Light Up My life - Debby Boone</li>
+      <li>Hey Jude - The Beatles</li>
+
+
+      </ol>
+
+      </section>
+      </div>
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
